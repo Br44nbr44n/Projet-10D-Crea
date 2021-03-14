35afdb3fc6de10a858e621b3896b35b0355cca4c
Hugo <boschath@gmail.com>
2021-03-14 23:01:25 +0100

    D2_Delta Green Tardif
    Il est un peu tard mais D2 fait

--- sketch.js
+++ sketch.js
@@ -1,12 +1,21 @@
+// draw a spinning box
+// with width, height and depth of 50
 function setup() {
   createCanvas(700, 700, WEBGL);
 }
 
 function draw() {
   background(0);
-  fill(240, 150, 150);
-  rotateX(frameCount * 0.04);
-  rotateY(frameCount * 0.04);
-  box((sin(frameCount/4) * 50) + 250);
-  
+  rotateX(frameCount * 0.01);
+  rotateZ(frameCount * 0.01);
+  stroke(  42, 191, 54  );
+  line(0,0,0,100,0,0);
+  line(100,0,0,100,100,0);
+  line(100,100,0,0,100,0);
+  line(0,100,0,0,0,0);
+  line(0,0,0,50,50,100);
+  line(100,0,0,50,50,100);
+  line(100,100,0,50,50,100);
+  line(0,100,0,50,50,100);
+
 }
